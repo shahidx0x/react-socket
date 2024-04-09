bf78c66d06af887b880223a51587c2ebe42f71ad
shahidx0x <dev.imshahid@gmail.com>
2024-04-10 02:59:39 +0600

    feat : isolated the error handler in utils

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const { errorHandlers } = require("./utils/errorHandlers");
 
 var app = express();
 
@@ -22,19 +22,9 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use(errorHandlers.createErrorMiddleware);
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.use(errorHandlers.handleErrorInDevelopment);
 
 module.exports = app;
